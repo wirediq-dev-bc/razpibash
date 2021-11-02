e766147c129dd068ed0900628b6330fe95638334
carv-cmd <bencarv1324@gmail.com>
2021-11-02 19:21:30 -0400

    PreCommit:  M get-posh.sh
    PostCommit:
    M  get-posh.sh

--- get-posh.sh
+++ get-posh.sh
@@ -5,12 +5,19 @@
 Usage () {
 	cat <<- EOF
 
-usage: ${0##*/} [ --get-omp ] [ --view ] [ --set <theme> ]
+usage: ${0##*/} [ --get-omp ] [ --view <theme> ] [ --set <theme> ]
 
 Flags:
-  --get-omp ) Install oh-my-posh and download themes
-  --view ) View all themes stored in ~/.poshthemes
-  --set ) Set shell theme from file in ~/.poshthemes
+  --get-omp ) 
+    Get oh-my-posh binaries and download/update themesets
+
+  --view ) 
+    See how a theme will look applied on your terminal.
+	If no arg passed to stdin, all themes in ~/.poshthemes are printed.
+	Single theme must already exist in ~/.poshthemes to apply.
+
+  --set ) 
+    Set oh-my-posh theme for shell environment
 
 EOF
 exit 1
@@ -21,54 +28,101 @@ Cmd_exists () {
 }
 
 Install_oh_my_posh () {
-	# Find your distro-pkg @ https://github.com/JanDeDobbeleer/oh-my-posh/releases/tag/v5.16.3
-	local ARM64_bin='https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v5.16.3/posh-linux-arm64'
+	# Find pkg @ "https://github.com/JanDeDobbeleer/oh-my-posh/releases"
 	local POSH_BIN='/usr/local/bin/oh-my-posh'
-	if Cmd_exists; then
-		oh-my-posh --help
-		return
-	else
-		sudo wget $ARM64_bin -O $POSH_BIN
-		sudo chmod +x $POSH_BIN
+	local OMP_VERSION="v5.16.3/posh-linux-arm64"
+	local SRC_BINARIES="https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/$OMP_VERSION"
+
+	if [ Cmd_exists -o -e "$POSH_BIN" ]; then
+		echo -e "\nUpdatingThemesUsedBy: `command -v oh-my-posh`"
 		return 0
 	fi
+
+	sudo wget $SRC_BINARIES -O $POSH_BIN
+	sudo chmod +x $POSH_BIN
+	return 0
 }
 
 Get_themes () {
 	# TODO `find ~/.poshthemes -newer` -> update
 	local THEME_URL='https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip'
-	local POSH="${HOME}/.poshthemes"
-	mkdir $POSH
-	wget $THEME_URL -O $POSH/themes.zip
-	unzip $POSH/themes.zip -d $POSH
-	chmod u+rw $POSH/*.json
-	rm $POSH/themes.zip
+	local POSHDIR="${HOME}/.poshthemes"
+
+	if [ ! -d "${POSHDIR}" ]; then
+		mkdir $POSHDIR || exit 1
+	fi
+
+	cd $POSHDIR || exit 1
+	if [[ "$(ls | grep -E '\.(json|zip)$')" ]]; then
+		echo "Cleaning: `pwd`"
+		rm ./*
+	fi
+
+	wget $THEME_URL -O $POSHDIR/themes.zip
+	unzip $POSHDIR/themes.zip -d $POSHDIR
+	chmod u+rw $POSHDIR/*.json
+	rm $POSHDIR/themes.zip
+	cd -
 }
 
 View_themes () {
-	if Cmd_exists; then
-		for style in ~/.poshthemes/*.omp.json; do
-			echo -e "${style}\n$(oh-my-posh --config $style --shell universal)\n"
-		done
+	Cmd_exists || exit 1
+	if [[ -n "${1}" && -r ~/.poshthemes/${1}.omp.json ]]; then
+		echo -e "\nOh-My-Posh.Style: ${1}"
+		echo -e "$(oh-my-posh --config ~/.poshthemes/${1}.omp.json --shell universal)\n"
+		return
 	fi
+
+	for style in ~/.poshthemes/*.omp.json; do
+		echo -e "${style}\n$(oh-my-posh --config $style --shell universal)\n"
+	done
+}
+
+Export_posh_theme () {
+	cat <<- EOF
+	# oh-my-posh-theme
+	SET_OMP_THEME="$1"
+	eval "\$(oh-my-posh --init --shell bash --config ~/.poshthemes/\${SET_OMP_THEME}.omp.json)"
+
+	EOF
 }
 
 Set_themes () {
-	local SET_THEME="${HOME}/.poshthemes/${1}.omp.json"
-	if [ ! -r "${SET_THEME}" ]; then
-		echo "${SET_THEME} doesnt exist!"
+	local SED_SEARCH="$(grep -E '^SET_OMP_THEME=' ${HOME}/.bashrc)"
+
+	if [ ! -r "${HOME}/.poshthemes/${1}.omp.json" ]; then
+		echo "Theme: <${1}> doesnt exist?!"; exit 1
+
+	elif [ -z "${SED_SEARCH}" ]; then
+		Export_posh_theme "$1" | tee -a $HOME/.bashrc
+
 	else
-		printf -v OMP '"$(oh-my-posh --init --shell bash --config %s)"' "$SET_THEME"
-		echo "eval $OMP" >> $HOME/.bashrc
-		source "${HOME}/.bashrc"
+		local SED_SWITCH="${SED_SEARCH%%=*}=\"${1}\""
+		sed -i s/$SED_SEARCH/$SED_SWITCH/ $HOME/.bashrc
 	fi
+	cat <<- 'EOF'
+	To apply changes do one of the following;
+
+	   * Logout and log back in
+	   * `source ~/.bashrc`
+	   * `. ~/.bashrc`
+
+	EOF
 }
 
 case "${1}" in
-	--get-omp )  Install_oh_my_posh && Get_themes ;;
-	--view )  View_themes ;;
-	--set )  shift; Set_themes "$1" ;;
-	* ) Usage ;;
+	--get-omp )  
+		Install_oh_my_posh && Get_themes 
+		;;
+	--set )  
+		shift; Set_themes "$1" 
+		;;
+	-v | --view )  
+		shift; View_themes "$1"
+		;;
+	* ) 
+		Usage 
+		;;
 esac
 
 
