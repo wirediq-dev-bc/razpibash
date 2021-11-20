c50c229914127e0d3d83a408224f9377bc01ecbf
carv-cmd <bencarv1324@gmail.com>
2021-11-20 11:19:03 -0500

    PreCommit:  M bpf_tools/get-bpf-tools.sh
    PostCommit:
    M  bpf_tools/get-bpf-tools.sh

--- bpf_tools/get-bpf-tools.sh
+++ bpf_tools/get-bpf-tools.sh
@@ -2,20 +2,6 @@
 
 # get-bpf-tools.sh: 
 
-Compile_bcc_original () {
-	# Compile BPF Compiler Collection (bcc) from source archive.
-	$sh_c 'git clone https://github.com/iovisor/bcc.git'
-	$sh_c 'mkdir bcc/build; cd bcc/build'
-	$sh_c 'cmake ..'
-	$sh_c 'make'
-	$sh_c 'sudo make install'
-	$sh_c 'cmake -DPYTHON_CMD=python3 ..'
-	$sh_c 'pushd src/python'
-	$sh_c 'make'
-	$sh_c 'sudo make install'
-	$sh_c 'popd'
-}
-
 Get_bpftrace () {
 	# Install bpftrace from upstream sources
 	$sh_c 'sudo apt-get install -y libbpffcc-dev'
@@ -33,13 +19,13 @@ Compile_bcc () {
 	make;
 	sudo make install;
 	popd
-	"
+	" || return 1
 }
 
 Build_bcc_toolchain () {
 	# Build necessary requirements for to compile bcc
 	DEPS="bison build-essential cmake flex git libedit-dev libllvm7 llvm-7-dev libclang-7-dev python zlib1g-dev libelf-dev libfl-dev python3-distutils"
-	$sh_c "sudo apt-get install -y --no-install-recommends $DEPS"
+	$sh_c "sudo apt-get install -y --no-install-recommends $DEPS" || return 1
 }
 
 Prep_host () {
@@ -55,7 +41,7 @@ sh_c='echo'
 RUN_IT="${RUN_IT:-}"
 if [ -n "$RUN_IT" ]; then
 	set -x
-	echo "sh_c='sh -c'"
+	sh_c='sh -c'
 	set +x
 fi
 
